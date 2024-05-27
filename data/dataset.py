3b3f612b53246146ba525d34399be5356e91a84b
Lucas da Silva Moreira <lucasmoreira@poli.ufrj.br>
2024-05-27 12:18:42 -0300

    adding vocab generation logic

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,8 @@
+from collections import Counter
 import re
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torchtext.vocab import build_vocab_from_iterator
 
 # Sentence Dataset
 class SentenceDataset(Dataset):
@@ -20,6 +22,26 @@ class SentenceDataset(Dataset):
     
     def __getitem__(self, idx):
         return self.sentences[idx]
+    
+class Vocabulary:
+    def __init__(self, sentences : Dataset):
+        self.sentences = sentences
+        self.vocabulary = self._build_vocabulary()
+
+    @staticmethod
+    def tokenize(text):
+        return text.lower().split()
+
+    # Despite not being explicit used in the paper
+    # It is a common practice to use a vocabulary sorted by frequency
+    def _build_vocabulary(self, sentences):
+        counter = Counter()
+        for text in sentences:
+            counter.update(self.tokenize(text))
+        # Sort tokens by frequency in descending order
+        sorted_tokens_by_freq = [token for token in counter.most_common()]
+        return build_vocab_from_iterator(sorted_tokens_by_freq)
+
 
 
 if __name__ == "__main__":
