e45c659f3083116b3494b50aa9060895f258012c
Lucas da Silva Moreira <lucasmoreira@poli.ufrj.br>
2024-11-28 08:27:40 -0300

    new layout

--- scripts/predict.py
+++
@@ -1,9 +0,0 @@
-import torch
-from common import init_model
-
-def predict(model, inputs, device="cpu"):
-    model.eval()
-    inputs = inputs.to(device)
-    with torch.no_grad():
-        outputs = model(inputs)
-    return outputs
